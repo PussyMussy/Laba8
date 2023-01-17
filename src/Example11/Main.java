a020068364172b40e5f65308d086b0c355f4ab16
PussyMussy <MaxWar228@mail.ru>
2023-01-17 21:00:54 +0500

    Initial commit

---
+++ src/Example11/Main.java
@@ -0,0 +1,48 @@
+package Example11;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Program {
+    public static void main(String[] args) {
+        try {
+            start();
+        } catch (Exception e) {
+            out.printf("Error - %e", e);
+        }
+    }
+
+    private static void start() throws Exception {
+        var scan = new Scanner(in);
+        var lines = new String[2];
+        var doubles = new Double[5];
+        for (int i = 0; i < lines.length; i++) {
+            out.print("Введите строку: ");
+            lines[i] = scan.nextLine();
+        }
+        for (int i = 0; i < doubles.length; i++) {
+            out.print("Введите вещ. число: ");
+            doubles[i] = scan.nextDouble();
+        }
+
+        FSWorker.writeArrayToFile("C:\\tmp\\f1.txt", false, lines);
+        FSWorker.writeArrayToFile("C:\\tmp\\f1.txt", true, doubles);
+
+        var fileInfo = FSWorker.readFileToArray("C:\\tmp\\f1.txt");
+        FSWorker.writeParamsToFile("C:\\tmp\\f2.txt", false, fileInfo.get(1));
+        var subFileInfo = fileInfo.subList(2, fileInfo.size());
+        for (String line: subFileInfo) {
+            try {
+                var number = Double.valueOf(line);
+                if (number % 2 == 0)
+                    FSWorker.writeParamsToFile("C:\\tmp\\f2.txt", true, number);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+        }
+    }
+}
